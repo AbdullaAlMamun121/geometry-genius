2ea3ed29a73a3feb3ed32ff7b689c6c66b66cb2a
Abdulla <hmdabdulla121@gmail.com>
2023-02-18 07:23:09 +0600

    rectangle calculation has been added

--- js/geometry.js
+++ js/geometry.js
@@ -1,13 +1,14 @@
-// Triangular functions
-function triangleGetValueById(elementId) {
+let incriment = 0;
+// Triangular catching value functions
+function getValueById(elementId) {
     const triangleInputField = document.getElementById(elementId);
     const triangleInputString = triangleInputField.value;
     const triangleInputValue = parseInt(triangleInputString);
     return triangleInputValue;
 
 }
-// triangle-title
-function triangleElementValueById(elementId) {
+// Triangle geting Title function
+function titleElementValueById(elementId) {
     const triangleFieldName = document.getElementById(elementId);
     const triangleString = triangleFieldName.innerText;
     return triangleString;
@@ -15,27 +16,36 @@ function triangleElementValueById(elementId) {
 
 // Triangle calculation button
 document.getElementById('triangle-calculate').addEventListener('click', function () {
-    
-    const triangleB = triangleGetValueById('triangle-input-b');
-    const triangleH = triangleGetValueById('triangle-input-h');
+    incriment++;
+    const triangleB = getValueById('triangle-input-b');
+    const triangleH = getValueById('triangle-input-h');
     const areaValue = 0.5 * triangleB * triangleH;
 
-   const triangleTitle = triangleElementValueById('triangle-title');
-    displayValueById(areaValue, triangleTitle);
+   const triangleTitle = titleElementValueById('triangle-title');
+    displayValueById(incriment,areaValue, triangleTitle);
 
 });
 
+// Rectangle calculation button
+document.getElementById('rectangle-calculation').addEventListener('click', function () {
+    incriment++;
+    const rectangleW = getValueById('rectangle-input-w');
+    const rectangleL = getValueById('rectangle-input-l');
+    const areaValue =  rectangleW * rectangleL;
 
+   const rectangleLTitle = titleElementValueById('rectangle-title');
+    displayValueById(incriment,areaValue, rectangleLTitle);
+
+});
 // common functions for display
-function displayValueById(name,value) {
+function displayValueById(incriment,value,name) {
     const container = document.getElementById('table-container')
     const tr = document.createElement('tr');
     tr.innerHTML = `
-     <tbody>
-    <td class="p-3">${1}</td>
-    <td class="p-3">${name}</td>
-    <td class="p-3">${value}</td>
-    <td><button class="btn btn-info py-0 size">Convert to m<sup>2</sup> </button></td>
+    <td class="p-2">${incriment}</td>
+    <td class="p-2">${value}<span>cm<sup>2</sup></span></td>
+    <td class="p-1">${name}</td>
+    <td><button class="btn btn-info py-0 size">Convert to m<sup>2</sup></button></td>
     `
     container.appendChild(tr);
  }
