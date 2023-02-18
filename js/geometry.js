89fe46770fccf036ca84731d5ce013a7a28c8647
Abdulla <hmdabdulla121@gmail.com>
2023-02-18 08:11:44 +0600

    another function rhombus is added

--- js/geometry.js
+++ js/geometry.js
@@ -1,5 +1,5 @@
 let incriment = 0;
-// Triangular catching value functions
+//  catching value functions
 function getValueById(elementId) {
     const triangleInputField = document.getElementById(elementId);
     const triangleInputString = triangleInputField.value;
@@ -7,11 +7,16 @@ function getValueById(elementId) {
     return triangleInputValue;
 
 }
-// Triangle geting Title function
+//  geting Title function
 function titleElementValueById(elementId) {
     const triangleFieldName = document.getElementById(elementId);
     const triangleString = triangleFieldName.innerText;
     return triangleString;
+}
+// Getting multiplication value
+function multiplicationValue(value1, value2) {
+    const result = value1 * value2;
+    return result;
 }
 
 // Triangle calculation button
@@ -19,24 +24,48 @@ document.getElementById('triangle-calculate').addEventListener('click', function
     incriment++;
     const triangleB = getValueById('triangle-input-b');
     const triangleH = getValueById('triangle-input-h');
-    const areaValue = 0.5 * triangleB * triangleH;
+    const areaValue = 0.5 * multiplicationValue(triangleB ,triangleH);
 
    const triangleTitle = titleElementValueById('triangle-title');
     displayValueById(incriment,areaValue, triangleTitle);
 
 });
 
-// Rectangle calculation button
-document.getElementById('rectangle-calculation').addEventListener('click', function () {
-    incriment++;
-    const rectangleW = getValueById('rectangle-input-w');
-    const rectangleL = getValueById('rectangle-input-l');
-    const areaValue =  rectangleW * rectangleL;
+//  Rectangle calculation button
+    document.getElementById('rectangle-calculation').addEventListener('click', function () {
+        incriment++;
+        const rectangleW = getValueById('rectangle-input-w');
+        const rectangleL = getValueById('rectangle-input-l');
+        const areaValue = multiplicationValue (rectangleW , rectangleL);
+    
+       const rectangleTitle = titleElementValueById('rectangle-title');
+        displayValueById(incriment,areaValue, rectangleTitle);
+    
+    });
 
-   const rectangleLTitle = titleElementValueById('rectangle-title');
-    displayValueById(incriment,areaValue, rectangleLTitle);
+    // Parallelogram calculation button
+    document.getElementById('parallelogram-calculation').addEventListener('click', function () {
+        incriment++;
+        const parallelogramB = getValueById('parallelogram-input-b');
+        const parallelogramH = getValueById('parallelogram-input-h');
+        const areaValue =  multiplicationValue(parallelogramB ,parallelogramH);
+    
+       const parallelogramTitle = titleElementValueById('rectangle-title');
+        displayValueById(incriment,areaValue, parallelogramTitle);
+    
+    });
+    // Rhombus calculation button
+    document.getElementById('rhombus-calculation').addEventListener('click', function () {
+        incriment++;
+        const rhombusD1 = getValueById('rhombus-input-d1');
+        const rhombusD2 = getValueById('rhombus-input-d2');
+        const areaValue = 0.5 * multiplicationValue(rhombusD1 ,rhombusD2);
+    
+       const rhombusTitle = titleElementValueById('rhombus-title');
+        displayValueById(incriment,areaValue, rhombusTitle);
+    
+    });
 
-});
 // common functions for display
 function displayValueById(incriment,value,name) {
     const container = document.getElementById('table-container')
